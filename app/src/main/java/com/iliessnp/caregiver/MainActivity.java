98e596bb11d7867319bc8b93a7c8a8c3d11d8b92
iliess-np <iliessnapolion@gmail.com>
2021-08-11 10:19:36 +0100

    added : google map and get gps_location from server

--- app/src/main/java/com/iliessnp/caregiver/MainActivity.java
+++ app/src/main/java/com/iliessnp/caregiver/MainActivity.java
@@ -1,16 +1,11 @@
 package com.iliessnp.caregiver;
 
-//Fetch dara
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.hardware.SensorEventListener;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListAdapter;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -31,16 +26,14 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity  {
-    //Fetch data
-    Button buttonfetch;
-    ListView listview;
-    ProgressDialog mProgressDialog;
+public class MainActivity extends AppCompatActivity {
     public static final String KEY_SENDERID = "sender_id";
-    String f_name;
-    String l_name;
-    String phone;
-    String senderId;
+    public static final String JSON_ARRAY = "result";
+    //Fetch data
+    Button btnFetchUser, btnFetchGPS, btnFetchAlert, btnShowMap;
+    TextView tv_fName, tv_lName, tv_phone, tv_accuracy, tv_gps_locationGPS, tv_reportTimeGPS, tv_alertType, tv_gps_locationAlert, tv_reportTimeAlert;
+    ProgressDialog mProgressDialog;
+    String f_name, l_name, phone, accuracy, gps_locationGPS, reportTimeGPS, alertType, gps_locationAlert, reportTimeAlert, senderId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,25 +45,92 @@ public class MainActivity extends AppCompatActivity  {
             senderId = intent.getExtras().getString("id");
         }
 
-        //Fetch data
-        buttonfetch = (Button) findViewById(R.id.btnfetch);
-        listview = (ListView) findViewById(R.id.listView);
 
-        buttonfetch.setOnClickListener(new View.OnClickListener() {
+        btnFetchUser = (Button) findViewById(R.id.btnFetchUser);
+        btnFetchGPS = (Button) findViewById(R.id.btnFetchGPS);
+        btnFetchAlert = (Button) findViewById(R.id.btnFetchAlert);
+        btnShowMap = (Button) findViewById(R.id.btnShowMap);
+
+        tv_fName = findViewById(R.id.tv_fname);
+        tv_lName = findViewById(R.id.tv_lname);
+        tv_phone = findViewById(R.id.tv_phone);
+        tv_accuracy = findViewById(R.id.tv_accuracy);
+        tv_gps_locationGPS = findViewById(R.id.tv_gps_locationGPS);
+        tv_reportTimeGPS = findViewById(R.id.tv_reportTimeGPS);
+        tv_alertType = findViewById(R.id.tv_alertType);
+        tv_gps_locationAlert = findViewById(R.id.tv_gps_locationAlert);
+        tv_reportTimeAlert = findViewById(R.id.tv_reportTimeAlert);
+
+        btnShowMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (gps_locationGPS != null) {
+                    Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                    intent.putExtra("gps_locationGPS", gps_locationGPS);
+                    startActivity(intent);
+                }else if (gps_locationAlert != null){
+                    Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                    intent.putExtra("gps_locationGPS", gps_locationAlert);
+                    startActivity(intent);
+                }else {
+                    Toast.makeText(MainActivity.this, "There no gps_location provided", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        btnFetchUser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 if (senderId.equals("")) {
                     Toast.makeText(MainActivity.this, "Please Enter Detail", Toast.LENGTH_SHORT).show();
                 } else {
-                    GetMatchData();
+                    String MATCHDATA_URL = "http://helptech29.000webhostapp.com/getDataCareGiver.php";
+                    String KEY_FNAME = "f_name";
+                    String KEY_LNAME = "l_name";
+                    String KEY_PHONE = "phone";
+                    int caseType = 1;
+                    GetMatchData(MATCHDATA_URL, KEY_FNAME, KEY_LNAME, KEY_PHONE, caseType);
+                }
+            }
+        });
+        btnFetchGPS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (senderId.equals("")) {
+                    Toast.makeText(MainActivity.this, "Please Enter Detail", Toast.LENGTH_SHORT).show();
+                } else {
+                    String MATCHDATA_URL = "http://helptech29.000webhostapp.com/getLocation.php";
+                    String KEY_FNAME = "accuracy";
+                    String KEY_LNAME = "gps_location";
+                    String KEY_PHONE = "reporttime";
+                    int caseType = 2;
+                    GetMatchData(MATCHDATA_URL, KEY_FNAME, KEY_LNAME, KEY_PHONE, caseType);
+                }
+            }
+        });
+        btnFetchAlert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (senderId.equals("")) {
+                    Toast.makeText(MainActivity.this, "Please Enter Detail", Toast.LENGTH_SHORT).show();
+                } else {
+                    String MATCHDATA_URL = "http://helptech29.000webhostapp.com/getAlertType.php";
+                    String KEY_FNAME = "alert";
+                    String KEY_LNAME = "gps_location";
+                    String KEY_PHONE = "reporttime";
+                    int caseType = 3;
+                    GetMatchData(MATCHDATA_URL, KEY_FNAME, KEY_LNAME, KEY_PHONE, caseType);
                 }
             }
         });
     }
 
     //Fetch data
-    private void GetMatchData() {
+    private void GetMatchData(String MATCHDATA_URL, String KEY_FNAME, String KEY_LNAME, String KEY_PHONE, int caseType) {
+
 
         mProgressDialog = new ProgressDialog(MainActivity.this);
         mProgressDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
@@ -82,15 +142,17 @@ public class MainActivity extends AppCompatActivity  {
         mProgressDialog.setProgressPercentFormat(null);
         mProgressDialog.show();
 
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, Config5.MATCHDATA_URL,
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, MATCHDATA_URL,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
                         if (response.trim().equals("success")) {
-                            showJSON(response);
+
+                            showJSON(response, KEY_FNAME, KEY_LNAME, KEY_PHONE, caseType);
                             mProgressDialog.dismiss();
                         } else {
-                            showJSON(response);
+
+                            showJSON(response, KEY_FNAME, KEY_LNAME, KEY_PHONE, caseType);
                             mProgressDialog.dismiss();
                         }
                     }
@@ -113,34 +175,61 @@ public class MainActivity extends AppCompatActivity  {
         requestQueue.add(stringRequest);
     }
 
-    private void showJSON(String response) {
+    private void showJSON(String response, String KEY_FNAME, String KEY_LNAME, String KEY_PHONE, int caseType) {
         ArrayList<HashMap<String, String>> list = new ArrayList<HashMap<String, String>>();
         try {
             JSONObject jsonObject = new JSONObject(response);
-            JSONArray result = jsonObject.getJSONArray(Config5.JSON_ARRAY);
+            JSONArray result = jsonObject.getJSONArray(JSON_ARRAY);
 
             for (int i = 0; i < result.length(); i++) {
                 JSONObject jo = result.getJSONObject(i);
-                f_name = jo.getString(Config5.KEY_FNAME);
-                l_name = jo.getString(Config5.KEY_LNAME);
-                phone = jo.getString(Config5.KEY_PHONE);
+                switch (caseType) {
+                    case 1:
+                        f_name = jo.getString(KEY_FNAME);
+                        l_name = jo.getString(KEY_LNAME);
+                        phone = jo.getString(KEY_PHONE);
+                        tv_fName.setText(f_name);
+                        tv_lName.setText(l_name);
+                        tv_phone.setText(phone);
+                        break;
+                    case 2:
+                        accuracy = jo.getString(KEY_FNAME);
+                        gps_locationGPS = jo.getString(KEY_LNAME);
+                        reportTimeGPS = jo.getString(KEY_PHONE);
 
-                final HashMap<String, String> employees = new HashMap<>();
-                employees.put(Config5.KEY_FNAME, "f_name = " + f_name);
-                employees.put(Config5.KEY_LNAME, "l_name = " + l_name);
-                employees.put(Config5.KEY_PHONE, "phone = " + phone);
+                        tv_accuracy.setText(accuracy);
+                        tv_gps_locationGPS.setText(gps_locationGPS);
+                        tv_reportTimeGPS.setText(reportTimeGPS);
+                        break;
+                    case 3:
+                        alertType = jo.getString(KEY_FNAME);
+                        gps_locationAlert = jo.getString(KEY_LNAME);
+                        reportTimeAlert = jo.getString(KEY_PHONE);
 
-                list.add(employees);
+                        tv_accuracy.setText(accuracy);
+                        tv_gps_locationAlert.setText(gps_locationAlert);
+                        tv_reportTimeAlert.setText(reportTimeAlert);
+                        break;
+
+                }
+
+
+//                final HashMap<String, String> employees = new HashMap<>();
+//                employees.put(KEY_FNAME, "f_name = " + f_name);
+//                employees.put(KEY_LNAME, "l_name = " + l_name);
+//                employees.put(KEY_PHONE, "phone = " + phone);
+//
+//                list.add(employees);
             }
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        ListAdapter adapter = new SimpleAdapter(
-                MainActivity.this, list, R.layout.list_item,
-                new String[]{Config5.KEY_FNAME, Config5.KEY_LNAME, Config5.KEY_PHONE},
-                new int[]{R.id.tv_fname, R.id.tv_lname, R.id.tv_phone});
-
-        listview.setAdapter(adapter);
+//        ListAdapter adapter = new SimpleAdapter(
+//                MainActivity.this, list, R.layout.list_item,
+//                new String[]{KEY_FNAME, KEY_LNAME, KEY_PHONE},
+//                new int[]{R.id.tv_fname, R.id.tv_lname, R.id.tv_phone});
+//
+//        listview.setAdapter(adapter);
     }
 
 }
---
+++ app/src/main/java/com/iliessnp/caregiver/MapsActivity.java
@@ -0,0 +1,58 @@
+package com.iliessnp.caregiver;
+
+import androidx.fragment.app.FragmentActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    String gps_locationGPS;
+    float lat,lon;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        Intent intent = getIntent();
+        if (intent.getExtras() != null) {
+            gps_locationGPS = intent.getExtras().getString("gps_locationGPS");
+
+            String[] gpsArray = gps_locationGPS.split(",");
+            lat = Float.parseFloat(gpsArray[0]);
+            lon = Float.parseFloat(gpsArray[1]);
+        }
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng userPosition = new LatLng(lat, lon);
+        mMap.addMarker(new MarkerOptions().position(userPosition).title("Marker in user Position"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userPosition, 16.0f));
+    }
+}
